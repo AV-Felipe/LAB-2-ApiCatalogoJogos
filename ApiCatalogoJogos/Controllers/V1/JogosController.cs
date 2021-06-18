ea91221177f38f82f635b885aa2880cdfe8d1280
AV-Felipe <aerogrado@gmail.com>
2021-06-18 12:31:53 -0300

    criação de interface para serviços e implementação de seus métodos no controller

--- ApiCatalogoJogos/Controllers/V1/JogosController.cs
+++ ApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -1,9 +1,11 @@
 ﻿using ApiCatalogoJogos.Models.InputModel;
 using ApiCatalogoJogos.Models.ViewModel;
+using ApiCatalogoJogos.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,41 +15,103 @@ namespace ApiCatalogoJogos.Controllers.V1
     [ApiController]
     public class JogosController : ControllerBase
     {
-        [HttpGet] //routting attribute, neste caso indica que temos um método get - ver: https://docs.microsoft.com/en-us/aspnet/web-api/overview/web-api-routing-and-actions/attribute-routing-in-web-api-2
-        public async Task<ActionResult<List<JogoViewModel>>> Obter() //Aqui temos um método assincrono, "Task" é um tipo de objeto que mantém tarefas em andamento, neste caso, o retorno de nosso método será dado quando tivermos o action result com uma listagem de objetos, a execução, por sua vez, estará livre para outras operações, mesmo antes de termos esse retorno - podemos ter muitas outras tasks rodando - ver: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/async/
+        
+        private readonly IJogoService _jogoService; //criamos uma propriedade, do tipo do nosso serviço, como private, já que será o asp.net quem terá a responsabilidade de instanciá-la
+
+        //construtor para nossa instância de IJogoService (a propriedade instanciada acima)
+        public JogosController(IJogoService jogoService)
         {
-            return Ok();
+            _jogoService = jogoService;
+        }
+
+
+
+
+        [HttpGet] //routting attribute, neste caso indica que temos um método get - ver: https://docs.microsoft.com/en-us/aspnet/web-api/overview/web-api-routing-and-actions/attribute-routing-in-web-api-2
+        public async Task<ActionResult<IEnumerable<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5) //aqui utilizamos data annotations nos parâmetros, fromQuery, nos diz que o parâmetro virá de uma requisição (api?123), assim, ambos os parâmetros que precisamos passar para o método obter, virão da requisição, sendo limitados pelos valores passados pelo Range (podemos ter até o máximo de inteiros para o número de páginas e, para cada página, 50 entradas)
+        {
+            var jogos = await _jogoService.Obter(pagina, quantidade); //aqui é onde, efetivamente, o serviço é evocado, recebendo os valores passados pela url para as variáveis "pagina"e "quantidade" e gerando uma lista de objetos DTO do tipo JogoViewModel
+
+            if (jogos.Count() == 0) //se essa lista de objetos do tipo JogoViewModel for vazia
+                return NoContent(); //retorna o código 204 (no content)
+
+            return Ok(jogos); //se existirem elementos na lista, retorna a lista
         }
 
         [HttpGet("{idJogo:guid}")] //passa o valor de idJogo, do método abaixo, para a rota do atributo
-        public async Task<ActionResult<JogoViewModel>> Obter(Guid idJogo)
+        public async Task<ActionResult<JogoViewModel>> Obter([FromRoute] Guid idJogo) //fromRoute obtem o valor diretamente da url (api/123)
         {
-            return Ok();
+            var jogo = await _jogoService.Obter(idJogo);
+
+            if (jogo == null)
+                return NoContent();
+
+            return Ok(jogo);
         }
 
         [HttpPost]
-        public async Task<ActionResult<JogoViewModel>> InserirJogo(JogoInputModel jogo) //este método, espera receber um objeto do tipo DTO
+        public async Task<ActionResult<JogoViewModel>> InserirJogo([FromBody] JogoInputModel jogoInputModel) //FromBody indica que o valor virá do corpo da requisição, via json, e será transformado, pelo pipeline do asp.net no nosso modelo de DTO (JogoInputModel)
         {
-            return Ok();
+            try
+            {
+                var jogo = await _jogoService.Inserir(jogoInputModel);
+
+                return Ok(jogo);
+            }
+            //catch (JogoJaCadastradoException ex)
+            catch(Exception ex)
+            {
+                return UnprocessableEntity("Já existe um jogo com este nome para esta produtora");
+            }
         }
 
         [HttpPut("{idJogo:guid}")]
-        public async Task<ActionResult> AtualizarJogo(Guid idJogo, JogoInputModel jogo) //o nosso task, aqui, só retornará um action result indicando sucesso ou falha
+        public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo, [FromBody] JogoInputModel jogoInputModel)
         {
-            return Ok();
+            try
+            {
+                await _jogoService.Atualizar(idJogo, jogoInputModel);
+
+                return Ok();
+            }
+            //catch (JogoNaoCadastradoException ex)
+            catch (Exception ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
         }
 
         //o httpPatch é similar ao put, mas nos permite atualizar apenas um campo do objeto, neste caso o preço
         [HttpPatch("{idJogo:guid}/preco/{preco:double")]
-        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco) 
+        public async Task<ActionResult> AtualizarJogo([FromRoute] Guid idJogo, [FromRoute] double preco)
         {
-            return Ok();
+            try
+            {
+                await _jogoService.Atualizar(idJogo, preco);
+
+                return Ok();
+            }
+            //catch (JogoNaoCadastradoException ex)
+            catch (Exception ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
         }
 
         [HttpDelete("{idJogo:guid}")]
-        public async Task<ActionResult> ApagarJogo(Guid idJogo, object jogo)
+        public async Task<ActionResult> ApagarJogo([FromRoute] Guid idJogo)
         {
-            return Ok();
+            try
+            {
+                await _jogoService.Remover(idJogo);
+
+                return Ok();
+            }
+            //catch (JogoNaoCadastradoException ex)
+            catch (Exception ex)
+            {
+                return NotFound("Não existe este jogo");
+            }
         }
     }
 }
---
+++ ApiCatalogoJogos/Services/IJogoService.cs
@@ -0,0 +1,25 @@
+﻿using ApiCatalogoJogos.Models.InputModel;
+using ApiCatalogoJogos.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.Services
+{
+   /*interface para estabelecer contratos com o controller - independente de como o serviço dos métodos abaixo será montado, já podemos montar
+    * toda a controller fazendo uso desses serviços, uma vez que já sabemos quais são os métodos (os aqui assinados) e que parâmetros
+    * passar para esses e que retornos esperar deles
+   */
+   //na interface possuímos apenas a "assinatura" de métodos, prpriedades, eventos e indexadores. A implementação desses se dá por meio de uma classe ou struct
+      
+    public interface IJogoService
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade); //para a otimização de recursos, prevendo uma lista muito grande, aqui trabalharemos com paginação
+        Task<JogoViewModel> Obter(Guid id);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid id, JogoInputModel jogo);
+        Task Atualizar(Guid id, double preco);
+        Task Remover(Guid id);
+    }
+}
